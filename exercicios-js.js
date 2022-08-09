4a901614766a63a87d42bbec5c5e3ca140ce0382
Vinícius Alexandre de Oliveira <73566303+vinialexandre@users.noreply.github.com>
2022-08-09 17:25:16 -0300

    init commit

---
+++ exercicios-js.js
@@ -0,0 +1,38 @@
+/* 1) Junte os items desse array e forme uma frase respeitando um espaco entre as palavras. */
+let listaString = ['tudo', 'está', 'separado']
+
+/* 2) Desenvolva uma funcção que receba 2 números, e retorne apenas os números pares entre o intervalo dos mesmos.
+Ex: 1 a 10
+saida: 2,4,6,8,10 */
+
+/* 3) Implemente a função numeroDePessoasMaiorDeIdade que recebe uma lista como argumento
+ e deve retornar o numero total de pessoas maior de idade 
+ deve retornar o numero de pessoas maior de idade */
+let listaDePessoas = [
+    {nome : 'gabriel', idade:22 },
+    {nome : 'bia', idade:18},
+    {nome : 'henrique', idade:21},
+    {nome : 'joão', idade:10},
+    {nome : 'giovana', idade:21},
+    {nome : 'marcelo', idade:9},
+    {nome : 'gustavo', idade:35},
+    {nome : 'vitoria', idade:7}
+  ]
+
+/*
+4) Crie uma função que receba um array/lista e retorne a lista ao contrário!
+*/
+let contagemNormal = ['um', 'dois', 'tres', 'quatro']
+//saida: quatro, tres, dois, um
+
+/*5)Implemente uma função que receba uma lista de números e retorne a soma total dos números.
+Ex: [1,2,3,4,5]
+saida: 15
+*/
+
+
+
+
+
+
+
